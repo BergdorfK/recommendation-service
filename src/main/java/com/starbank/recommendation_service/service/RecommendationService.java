cbc32062b6412596963944fc97246752ec7922bc
BergdorfK <bergdorf.kolos@gmail.com>
2025-09-11 13:26:52 +0300

    Revert "Develop pavel"

--- src/main/java/com/starbank/recommendation_service/service/RecommendationService.java
+++ src/main/java/com/starbank/recommendation_service/service/RecommendationService.java
@@ -2,50 +2,36 @@ package com.starbank.recommendation_service.service;
 
 import com.starbank.recommendation_service.dto.RecommendationDto;
 import com.starbank.recommendation_service.dto.RecommendationResponse;
-import com.starbank.recommendation_service.dynamic.service.DynamicRuleService;
 import com.starbank.recommendation_service.model.UserFinancialData;
 import com.starbank.recommendation_service.repository.UserRepository;
 import com.starbank.recommendation_service.rules.RecommendationRuleSet;
-import org.springframework.dao.DataAccessException;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
 public class RecommendationService {
 
-    private final List<RecommendationRuleSet> recommendationRuleSets;
+    private final List<RecommendationRuleSet> rules;
     private final UserRepository userRepository;
-    private final DynamicRuleService dynamicRuleService;
 
-    public RecommendationService(List<RecommendationRuleSet> recommendationRuleSets,
-                                 UserRepository userRepository,
-                                 DynamicRuleService dynamicRuleService) {
-        this.recommendationRuleSets = recommendationRuleSets;
+    public RecommendationService(List<RecommendationRuleSet> rules, UserRepository userRepository) {
+        this.rules = rules;
         this.userRepository = userRepository;
-        this.dynamicRuleService = dynamicRuleService;
     }
 
     public RecommendationResponse getRecommendationResponse(UUID userId) {
         UserFinancialData financialData = userRepository.getUserFinancialData(userId);
 
-        List<RecommendationDto> recommendations = new ArrayList<>();
-
-        // Применяем фиксированные правила
-        for (RecommendationRuleSet ruleSet : recommendationRuleSets) {
-            ruleSet.apply(financialData).ifPresent(recommendations::add);
-        }
-
-        // Применяем динамические правила
-        List<RecommendationDto> dynamicRecommendations = dynamicRuleService.evaluateDynamic(userId);
-        recommendations.addAll(dynamicRecommendations);
+        List<RecommendationDto> recommendations = rules.stream()
+                .map(rule -> rule.apply(financialData))
+                .flatMap(Optional::stream)
+                .collect(Collectors.toList());
 
         return new RecommendationResponse(userId.toString(), recommendations);
     }
+
 }
-
-
-
