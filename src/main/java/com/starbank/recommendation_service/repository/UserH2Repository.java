000ade535db07e931c0ff4b5819fca6bf0e8b161
BergdorfK <bergdorf.kolos@gmail.com>
2025-09-29 23:48:58 +0300

    fix

--- src/main/java/com/starbank/recommendation_service/repository/UserH2Repository.java
+++ src/main/java/com/starbank/recommendation_service/repository/UserH2Repository.java
@@ -1,13 +1,10 @@
 package com.starbank.recommendation_service.repository;
 
 import com.starbank.recommendation_service.model.UserH2;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
-public interface UserH2Repository extends JpaRepository<UserH2, Long> {
+public interface UserH2Repository {
     Optional<UserH2> findByUsername(String username);
-    Optional<UserH2> findByUserId(String userId); // Может понадобиться для поиска по UUID
+    Optional<UserH2> findByUserId(String userId);
 }
